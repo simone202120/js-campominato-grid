f43c5e94beb3d79319983c6331ba781022a2a777
Simone Fortunato <frtsmn94@gmail.com>
2022-07-13 17:36:01 +0200

    bonus

--- js/main.js
+++ js/main.js
@@ -1,9 +1,36 @@
 let contenitore=  document.getElementById('contenitore');
+let play = document.getElementById("play");
+let difficoltà = document.getElementById('difficult').value;
+let n;
+if(difficoltà.value == 1){
+    n=100;
+}else if( difficoltà.value == 2){
+    n==81;
+}else if( difficoltà.value == 3){
+    n==49;
+}
+play.addEventListener('click', function(){
+    creaquadrati(n);
+    }
+);
 
-function creaquadrti(){
-    for(let i=0; i<=100; i++){
+
+
+
+function creaquadrati(numero){
+    console.log('SONO DENTRO LA FUNCTION')
+    for(let i=1; i<=numero; i++){
         const creaquadrato= document.createElement('div');
-        creaquadrato.classList.add('square');
+        console.log('SONO DENTRO LA FOR')
+        creaquadrato.classList.add('quadrato');
+        creaquadrato.innerHTML = i;
         contenitore.append(creaquadrato);
+
+        creaquadrato.addEventListener('click',
+            function (){
+                creaquadrato.classList.toggle('azzurro');
+                console.log(i)
+             }
+        )
     }
 }
